5a08a35d6d436ab451a95b61260da79b0c36c020
rangasanju <rangasanju@gmail.com>
2019-09-12 17:12:47 +0530

    Modified configuration for generating war to be deployed on tomcat

--- src/main/java/com/infyniteloop/rrms/RrmsApplication.java
+++ src/main/java/com/infyniteloop/rrms/RrmsApplication.java
@@ -2,9 +2,10 @@ package com.infyniteloop.rrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RrmsApplication {
+public class RrmsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RrmsApplication.class, args);
